334e6850318b7b410e85b828beaceac845a56918
ThMoraes <89168163+ThMoraes@users.noreply.github.com>
2022-01-20 13:58:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -100,21 +100,28 @@ function acessibilidadeConteudo(){
     }
 };
 
-/* function altoContraste(){
-    var css = document.getElementById('css');
-    var str = $(css).attr('href');
+function temaEscuro(){
+    var str = document.getElementById('css').getAttribute("href");
     var arr = str.split(" ");
     var res = arr[0];
 
-    if( res == "altoContraste.css"){
+    if( res == "temaEscuro.css"){
         var css = document.getElementById('css');
         css.href="style.css";
+        
+        var iconeAcessibilidade = document.getElementById('iconeAcessibilidade1');
+        iconeAcessibilidade.innerHTML = "toggle_off";
+        iconeAcessibilidade.className = "material-icons iconeAcessibilidade iconeAcessibilidade1Off";
     }
     else if( res == "style.css"){
         var css = document.getElementById('css');
-        css.href="altoContraste.css";
+        css.href="temaEscuro.css";
+
+        var iconeAcessibilidade = document.getElementById('iconeAcessibilidade1');
+        iconeAcessibilidade.innerHTML = "toggle_on";
+        iconeAcessibilidade.className = "material-icons iconeAcessibilidade iconeAcessibilidade1On";
     }
-} */
+}
 
 function altoContraste(){
     var str = document.getElementById('css').getAttribute("href");
@@ -124,10 +131,18 @@ function altoContraste(){
     if( res == "altoContraste.css"){
         var css = document.getElementById('css');
         css.href="style.css";
+        
+        var iconeAcessibilidade = document.getElementById('iconeAcessibilidade2');
+        iconeAcessibilidade.innerHTML = "toggle_off";
+        iconeAcessibilidade.className = "material-icons iconeAcessibilidade iconeAcessibilidade2Off";
     }
     else if( res == "style.css"){
         var css = document.getElementById('css');
         css.href="altoContraste.css";
+
+        var iconeAcessibilidade = document.getElementById('iconeAcessibilidade2');
+        iconeAcessibilidade.innerHTML = "toggle_on";
+        iconeAcessibilidade.className = "material-icons iconeAcessibilidade iconeAcessibilidade2On";
     }
 }
 
@@ -177,29 +192,35 @@ $('#linkBotaoInicioConteudo').click(function(e){
 });
 
 $('#menuConteudo1').hover(function(){
-    $('#menuConteudo1 .conteudoNomeMenuConteudo').toggleClass('conteudoNomeMenuConteudoHover');
+    $('#menuConteudo1 .conteudoNomeMenuConteudo').toggleClass('conteudoNomeMenuConteudoOver');
 });
 $('#menuConteudo2').hover(function(){
-    $('#menuConteudo2 .conteudoNomeMenuConteudo').toggleClass('conteudoNomeMenuConteudoHover');
+    $('#menuConteudo2 .conteudoNomeMenuConteudo').toggleClass('conteudoNomeMenuConteudoOver');
 });
 $('#menuConteudo3').hover(function(){
-    $('#menuConteudo3 .conteudoNomeMenuConteudo').toggleClass('conteudoNomeMenuConteudoHover');
+    $('#menuConteudo3 .conteudoNomeMenuConteudo').toggleClass('conteudoNomeMenuConteudoOver');
 });
 $('#menuConteudo4').hover(function(){
-    $('#menuConteudo4 .conteudoNomeMenuConteudo').toggleClass('conteudoNomeMenuConteudoHover');
+    $('#menuConteudo4 .conteudoNomeMenuConteudo').toggleClass('conteudoNomeMenuConteudoOver');
 });
 $('#menuConteudo5').hover(function(){
-    $('#menuConteudo5 .conteudoNomeMenuConteudo').toggleClass('conteudoNomeMenuConteudoHover');
+    $('#menuConteudo5 .conteudoNomeMenuConteudo').toggleClass('conteudoNomeMenuConteudoOver');
 });
 $('#menuConteudo6').hover(function(){
-    $('#menuConteudo6 .conteudoNomeMenuConteudo').toggleClass('conteudoNomeMenuConteudoHover');
+    $('#menuConteudo6 .conteudoNomeMenuConteudo').toggleClass('conteudoNomeMenuConteudoOver');
 });
 $('#menuConteudo7').hover(function(){
-    $('#menuConteudo7 .conteudoNomeMenuConteudo').toggleClass('conteudoNomeMenuConteudoHover');
+    $('#menuConteudo7 .conteudoNomeMenuConteudo').toggleClass('conteudoNomeMenuConteudoOver');
 });
 $('#menuConteudo8').hover(function(){
-    $('#menuConteudo8 .conteudoNomeMenuConteudo').toggleClass('conteudoNomeMenuConteudoHover');
+    $('#menuConteudo8 .conteudoNomeMenuConteudo').toggleClass('conteudoNomeMenuConteudoOver');
 });
 $('#menuConteudo9').hover(function(){
-    $('#menuConteudo9 .conteudoNomeMenuConteudo').toggleClass('conteudoNomeMenuConteudoHover');
+    $('#menuConteudo9 .conteudoNomeMenuConteudo').toggleClass('conteudoNomeMenuConteudoOver');
 });
+
+
+/* $('#acessibilidadeConteudo1').hover(function(){
+    $('.acessibilidadeConteudo').toggleClass('acessibilidadeConteudoOver');
+});
+ */
